2cde09ed426737a7c0cdacd4986e19e68b732af4
Simon Bourne <simonbourne@gmail.com>
2023-09-28 19:34:26 +0100

    Effects

--- experiments/desugared-effects/src/main.rs
+++ experiments/desugared-effects/src/main.rs
@@ -1,3 +1,6 @@
+// The function is compiled to delimited continuations.
+// The action enum can hold arguments for any action that can happen.
+
 enum Action {
     Exception(ExceptionAction),
 }
@@ -10,36 +13,70 @@ struct Exception {
     message: String,
 }
 
-#[derive(Default)]
-struct EffectfulState {
-    current_action: Option<Action>,
-    current_continuation: Option<fn(&mut EffectfulState)>,
+type Continuation = fn(&mut EffectfulState) -> Option<ActionHandler>;
+type ActionHandler = fn(&mut EffectfulState);
+
+pub struct EffectfulState {
+    throw_handler: ActionHandler,
+    action_args: Option<Action>,
+    continuation: Continuation,
 }
 
-fn effectful_enter_try(state: &mut EffectfulState) {
-    println!("Enter try");
-    state.current_action = Some(Action::Exception(ExceptionAction::Throw(Exception {
-        message: "Testing".to_string(),
-    })));
-    state.current_continuation = Some(effectful_after_throw);
-}
-
-fn effectful_after_throw(_state: &mut EffectfulState) {
-    panic!("After throw");
-}
-
-fn catch(state: &mut EffectfulState) {
-    if let Some(action) = &state.current_action {
-        match action {
-            Action::Exception(ExceptionAction::Throw(Exception { message })) => {
-                println!("Exception: {message}")
-            }
+impl EffectfulState {
+    pub fn new(throw_handler: ActionHandler) -> Self {
+        Self {
+            throw_handler,
+            action_args: None,
+            continuation: effectful::enter_try,
         }
     }
 }
 
+mod effectful {
+    // An effectful function, split into delimited continuations.
+    use crate::{Action, ActionHandler, EffectfulState, Exception, ExceptionAction};
+
+    pub fn enter_try(state: &mut EffectfulState) -> Option<ActionHandler> {
+        println!("Enter try");
+        state.action_args = Some(Action::Exception(ExceptionAction::Throw(Exception {
+            message: "Testing".to_string(),
+        })));
+        state.continuation = after_throw;
+        Some(state.throw_handler)
+    }
+
+    pub fn after_throw(_state: &mut EffectfulState) -> Option<ActionHandler> {
+        panic!("After throw");
+    }
+
+    pub fn after_exception_handler(_state: &mut EffectfulState) -> Option<ActionHandler> {
+        println!("Finished");
+        None
+    }
+}
+
+mod handlers {
+    use crate::{effectful, Action, EffectfulState, Exception, ExceptionAction};
+
+    pub fn throw(state: &mut EffectfulState) {
+        if let Some(Action::Exception(ExceptionAction::Throw(Exception { message }))) =
+            &state.action_args
+        {
+            println!("Exception: {message}")
+        } else {
+            panic!("Bad arguments");
+        }
+
+        // TODO: Store this in the exception handler state, rather than hard wiring it.
+        // TODO: Unwind the stack.
+        state.continuation = effectful::after_exception_handler;
+    }
+}
+
 fn main() {
-    let mut state = EffectfulState::default();
-    effectful_enter_try(&mut state);
-    catch(&mut state);
+    let mut state = EffectfulState::new(handlers::throw);
+
+    while let Some(handler) = (state.continuation)(&mut state) {
+        handler(&mut state);
+    }
 }
