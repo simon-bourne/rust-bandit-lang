0374282c6622530ac5ea79323484d9a9a89b3113
Simon Bourne <simonbourne@gmail.com>
2023-09-28 17:47:15 +0100

    Handle exceptions

--- experiments/desugared-effects/src/main.rs
+++ experiments/desugared-effects/src/main.rs
@@ -1,9 +1,26 @@
+enum Action {
+    Exception(ExceptionAction),
+}
+
+enum ExceptionAction {
+    Throw(Exception),
+}
+
+struct Exception {
+    message: String,
+}
+
+#[derive(Default)]
 struct EffectfulState {
+    current_action: Option<Action>,
     current_continuation: Option<fn(&mut EffectfulState)>,
 }
 
 fn effectful_enter_try(state: &mut EffectfulState) {
     println!("Enter try");
+    state.current_action = Some(Action::Exception(ExceptionAction::Throw(Exception {
+        message: "Testing".to_string(),
+    })));
     state.current_continuation = Some(effectful_after_throw);
 }
 
@@ -11,8 +28,18 @@ fn effectful_after_throw(_state: &mut EffectfulState) {
     panic!("After throw");
 }
 
-fn handle_exceptions(state: &mut EffectfulState) {}
+fn catch(state: &mut EffectfulState) {
+    if let Some(action) = &state.current_action {
+        match action {
+            Action::Exception(ExceptionAction::Throw(Exception { message })) => {
+                println!("Exception: {message}")
+            }
+        }
+    }
+}
 
 fn main() {
-    println!("Hello world")
+    let mut state = EffectfulState::default();
+    effectful_enter_try(&mut state);
+    catch(&mut state);
 }
