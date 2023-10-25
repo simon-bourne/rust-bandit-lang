2fc3f07624f41fee5898ab5b5df07fbef31fc29c
Simon Bourne <simonbourne@gmail.com>
2023-10-25 19:25:30 +0100

    Layout parser

--- packages/compiler/src/main.rs
+++ packages/compiler/src/main.rs
@@ -1,87 +1,68 @@
 use chumsky::{
     prelude::*,
-    text::{ascii::ident, inline_whitespace, Char},
+    text::{inline_whitespace, newline},
 };
 
-#[derive(Clone, Debug, PartialEq)]
-enum LayoutToken<'src> {
-    Indentation(&'src str),
-    Token(Token<'src>),
+#[derive(Clone, Debug)]
+enum Stmt<'src> {
+    Expr(Vec<&'src str>),
+    Do(Vec<Stmt<'src>>),
 }
 
-#[derive(Clone, Debug, PartialEq)]
-enum Delimiter {
-    Parentheses,
-    Bracket,
-    Brace,
-}
-
-#[derive(Clone, Debug, PartialEq)]
-enum Token<'src> {
-    Do,
-    Open(Delimiter),
-    Close(Delimiter),
-    Identifier(&'src str),
-    LineEnd,
-}
-
-type Spanned<T> = (T, SimpleSpan<usize>);
-
-fn lexer<'src>(
-) -> impl Parser<'src, &'src str, Vec<Spanned<LayoutToken<'src>>>, extra::Err<Rich<'src, char>>> {
-    let identifier = ident().map(|ident| match ident {
-        "do" => Token::Do,
-        _ => Token::Identifier(ident),
-    });
-
-    let delimiter = one_of("()[]{}").map(|c| match c {
-        '(' => Token::Open(Delimiter::Parentheses),
-        ')' => Token::Close(Delimiter::Parentheses),
-        '[' => Token::Open(Delimiter::Bracket),
-        ']' => Token::Close(Delimiter::Bracket),
-        '{' => Token::Open(Delimiter::Brace),
-        '}' => Token::Close(Delimiter::Brace),
-        _ => unreachable!(),
-    });
-
-    let line_end = just(';').to(Token::LineEnd);
-    let single_indentation = text::newline().ignore_then(
-        any()
-            .filter(|c: &char| c.is_inline_whitespace())
+fn parser<'a>() -> impl Parser<'a, &'a str, Vec<Stmt<'a>>> {
+    let block = recursive(|block| {
+        let indent = just(' ')
             .repeated()
-            .to_slice()
-            .map(LayoutToken::Indentation),
-    );
-    let indentation = single_indentation.foldl(single_indentation.repeated(), |_x, y| y);
-    let token = indentation.or(choice((line_end, delimiter, identifier))
-        .padded_by(inline_whitespace())
-        .map(LayoutToken::Token));
+            .configure(|cfg, parent_indent| cfg.exactly(*parent_indent));
+        // TODO: Ignore blank lines
+        let word_separator = inline_whitespace().then_ignore(
+            newline()
+                .then_ignore(indent)
+                .then_ignore(inline_whitespace().at_least(1))
+                .repeated()
+                .at_most(1),
+        );
+        let expr = just("expr")
+            .separated_by(word_separator)
+            .collect::<Vec<_>>();
 
-    token
-        .map_with(|tok, e| (tok, e.span()))
-        .recover_with(skip_then_retry_until(any().ignored(), end()))
-        .repeated()
-        .collect()
+        let expr_stmt = expr.then_ignore(text::newline()).map(Stmt::Expr);
+        let open_layout_block = just("do")
+            .then(text::newline())
+            .ignore_then(block)
+            .map(Stmt::Do);
+        let stmt = expr_stmt.or(open_layout_block);
+
+        text::whitespace()
+            .count()
+            .ignore_with_ctx(stmt.separated_by(indent).collect())
+    });
+
+    block.with_ctx(0)
 }
-
-// TODO: Write layout parser `[Spanned<LayoutToken>]` to `[Spanned<Token>]`
-// TODO: Write parser of `[Spanned<Token>]`
 
 fn main() {
-    let tokens = lexer().parse(
-        r#"identifier
-identifier
+    // TODO: Handle `do` at end of line
+    // TODO: Handle inline `do`
+    // TODO: Brackets
+    // TODO: Handle do in middle of expression:
+    // do
+    //         expr
+    //     expr
 
-identifier do
-    identifier
-
+    let stmts = parser().padded().parse(
+        r#"
+expr
+expr
+do
+    expr expr
+     expr
     do
-        identifier
-        identifier
-    identifier
-identifier
+        expr expr
+        expr
+exprexpr
 "#,
     );
-    println!("{:#?}", tokens.output());
-    println!("{:?}", tokens.errors().collect::<Vec<_>>());
+    println!("{:#?}", stmts.output());
+    println!("{:?}", stmts.errors().collect::<Vec<_>>());
 }
