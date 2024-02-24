1598ee459befff13ebf22c433f5a163dd8b103ae
Simon Bourne <simonbourne@gmail.com>
2024-02-24 17:44:27 +0000

    Very basic function parser

--- packages/compiler/src/parse.rs
+++ packages/compiler/src/parse.rs
@@ -1,12 +1,11 @@
 use chumsky::{
-    combinator::DelimitedBy,
     extra, input,
     prelude::{Cheap, Rich},
-    primitive::{just, Just},
-    select, IterParser, Parser,
+    primitive::{self, Just},
+    select, Boxed, IterParser, Parser,
 };
 
-use crate::lex::{Delimiter, Span, Token};
+use crate::lex::{Delimiter, Keyword, Span, Token};
 
 pub type SpannedInput<'src, T> = input::SpannedInput<T, Span, &'src [(T, Span)]>;
 
@@ -22,15 +21,8 @@ pub enum Item {
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Function {
-    pub name: Ident,
-    pub body: Vec<Line>,
+    pub name: Identifier,
 }
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Line(Ident);
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Ident(Span);
 
 pub type FastError<'src> = extra::Err<Cheap<Span>>;
 pub type RichError<'src> = extra::Err<Rich<'src, Token, Span>>;
@@ -40,14 +32,11 @@ pub type JustToken<'src> = Just<Token, SpannedInput<'src, Token>, RichError<'src
 pub trait TTParser<'src, Output>:
     Parser<'src, SpannedInput<'src, Token>, Output, RichError<'src>>
 {
-    fn delimited(
-        self,
-        delimiter: Delimiter,
-    ) -> DelimitedBy<Self, JustToken<'src>, JustToken<'src>, Token, Token>
+    fn skip_line_ends(self) -> Boxed<'src, 'src, SpannedInput<'src, Token>, Output, RichError<'src>>
     where
-        Self: Sized,
+        Self: Sized + 'src,
     {
-        self.delimited_by(just(Token::Open(delimiter)), just(Token::Close(delimiter)))
+        self.then_ignore(line_end().repeated()).boxed()
     }
 }
 
@@ -56,20 +45,64 @@ impl<'src, Output, T> TTParser<'src, Output> for T where
 {
 }
 
-fn ident<'src>() -> impl TTParser<'src, Ident> + Copy {
-    select! { Token::Identifier = ext => Ident(ext.span()) }
+macro_rules! lexeme {
+    ($name:ident, $token:ident) => {
+        #[derive(Clone, Debug, Eq, PartialEq)]
+        pub struct $token(Span);
+
+        fn $name<'src>() -> impl TTParser<'src, $token> + Copy {
+            select! { Token::$token = ext => $token(ext.span()) }
+        }
+    };
+}
+
+lexeme!(ident, Identifier);
+lexeme!(operator, Operator);
+
+macro_rules! token {
+    ($name:ident, $token:ident) => {
+        fn $name<'src>() -> impl TTParser<'src, ()> + Copy {
+            select! { Token::$token => () }
+        }
+    };
+}
+
+token!(line_end, LineEnd);
+token!(statement_end, StatementEnd);
+
+fn kw<'src>(keyword: Keyword) -> impl TTParser<'src, ()> {
+    primitive::select(move |x, _| (x == Token::Keyword(keyword)).then_some(())).skip_line_ends()
+}
+
+fn open<'src>(delimiter: Delimiter) -> impl TTParser<'src, ()> {
+    primitive::select(move |x, _| (x == Token::Open(delimiter)).then_some(())).skip_line_ends()
+}
+
+fn close<'src>(delimiter: Delimiter) -> impl TTParser<'src, ()> {
+    primitive::select(move |x, _| (x == Token::Close(delimiter)).then_some(()))
 }
 
 pub fn parser<'src>() -> impl TTParser<'src, AST> {
-    let ident = ident();
-    let line = ident.map(Line);
-    let body = line.repeated().collect();
-    let function = ident
-        .then(body.delimited(Delimiter::Braces))
-        .map(|(name, body)| Function { name, body });
-    let item = function.map(Item::Function);
+    line_end().repeated().ignore_then(
+        function()
+            .map(Item::Function)
+            .repeated()
+            .collect()
+            .map(|items| AST { items }),
+    )
+}
 
-    item.repeated().collect().map(|items| AST { items })
+fn function<'src>() -> impl TTParser<'src, Function> {
+    let name = ident()
+        .skip_line_ends()
+        .then_ignore(open(Delimiter::Parentheses))
+        .then_ignore(close(Delimiter::Parentheses))
+        .skip_line_ends()
+        .then_ignore(operator())
+        .skip_line_ends()
+        .then_ignore(kw(Keyword::Do))
+        .then_ignore(statement_end().skip_line_ends());
+    name.map(|name| Function { name })
 }
 
 #[cfg(test)]
@@ -78,17 +111,15 @@ mod tests {
 
     use crate::{
         lex::{Span, Token},
-        parse::{parser, Function, Ident, Item, Line, AST},
+        parse::{parser, Function, Identifier, Item, AST},
     };
 
     #[test]
     fn basic() {
         // TODO: indoc
         const SRC: &str = r#"
-my_function {
-    call1;
-    call2;
-}
+my_function() = do
+;
 "#;
 
         let tokens = Token::tokens(SRC).collect::<Vec<_>>();
@@ -100,11 +131,7 @@ my_function {
             ast.unwrap(),
             AST {
                 items: vec![Item::Function(Function {
-                    name: Ident(Span::new(1, 12)),
-                    body: vec![
-                        Line(Ident(Span::new(19, 24))),
-                        Line(Ident(Span::new(30, 35)))
-                    ]
+                    name: Identifier(Span::new(1, 12)),
                 })]
             }
         )
