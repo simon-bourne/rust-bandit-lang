e61509f8fce32bdd9cfdf686371e2afa1ea52847
Simon Bourne <simonbourne@gmail.com>
2024-03-09 08:49:01 +0000

    Layout lexer benchmarks

--- packages/compiler/benches/lexer.rs
+++ packages/compiler/benches/lexer.rs
@@ -1,5 +1,6 @@
-use bandit_compiler::lex::Token;
+use bandit_compiler::lex::{SrcToken, Token};
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use logos::Logos;
 
 pub fn basic(c: &mut Criterion) {
     let mut input = String::new();
@@ -33,14 +34,31 @@ expr do expr expr do
 
     group.bench_function("logos", |b| {
         b.iter(|| {
-            let lexer = Token::tokens(&input);
+            let lexer = Token::lexer(&input);
 
             for token in lexer {
-                assert!(token.0 != Token::Error);
+                assert!(token.is_ok());
                 black_box(&token);
             }
         })
     });
+    group.bench_function("logos-errors", |b| {
+        b.iter(|| {
+            consume_lexer(Token::tokens(&input));
+        })
+    });
+    group.bench_function("layout", |b| {
+        b.iter(|| {
+            consume_lexer(Token::layout(&input));
+        })
+    });
+}
+
+fn consume_lexer<'a>(lexer: impl Iterator<Item = SrcToken<'a>>) {
+    for token in lexer {
+        assert!(token.0 != Token::Error);
+        black_box(&token);
+    }
 }
 
 criterion_group!(benches, basic);
