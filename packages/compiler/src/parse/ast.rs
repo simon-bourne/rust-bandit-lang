27719dab704f94ed38b56d8664a641b69e22d3b0
Simon Bourne <simonbourne@gmail.com>
2024-05-19 12:08:25 +0100

    AST for `data` declarations

--- packages/compiler/src/parse/ast.rs
+++ packages/compiler/src/parse/ast.rs
@@ -17,6 +17,36 @@ pub struct Function<'src> {
     pub name: Identifier<'src>,
 }
 
+/// A `data` declaration
+///
+/// ```bandit
+/// data MyType (a : Type -> Type) (b : Type) c where ...
+/// ```
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct DataDeclaration<'src> {
+    pub name: Identifier<'src>,
+    pub parameters: Vec<Expression<'src>>,
+    pub where_clause: WhereClause<'src>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Expression<'src> {
+    Variable(Identifier<'src>),
+    Operator {
+        name: OperatorName<'src>,
+        arguments: Vec<Expression<'src>>,
+    },
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum OperatorName<'src> {
+    Apply,
+    Named(Identifier<'src>),
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Constructor)]
+pub struct WhereClause<'src>(Vec<Expression<'src>>);
+
 #[derive(Clone, Debug, Eq, PartialEq, Constructor)]
 pub struct Identifier<'src> {
     pub name: &'src str,
