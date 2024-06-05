f1b7c6207939fdaa11a157f9fec52cc8a632bd14
Simon Bourne <simonbourne@gmail.com>
2024-06-05 11:22:05 +0100

    Data for type inference

--- packages/type-inference/src/lib.rs
+++ packages/type-inference/src/lib.rs
@@ -1,1 +1,51 @@
+// TODO: Deny unused
+#![allow(unused)]
+use bandit_parser::ast;
 
+pub trait Annotation<'src> {
+    type Kind;
+    type Type;
+    type TypeIdentifier;
+}
+
+pub struct Source;
+
+impl<'src> Annotation<'src> for Source {
+    type Kind = Option<Kind>;
+    type Type = Option<QuantifiedType<'src, Self>>;
+    type TypeIdentifier = ast::Identifier<'src>;
+}
+
+pub struct Context<'src, A: Annotation<'src>> {
+    data: Vec<DataDeclaration<'src, A>>,
+}
+
+pub struct QuantifiedType<'src, A: Annotation<'src>> {
+    parameters: Vec<TypeParameter<'src, A>>,
+    typ: Type<A::TypeIdentifier, Self>,
+}
+
+pub struct Kind(Type<(), Self>);
+
+struct DataDeclaration<'src, A: Annotation<'src>> {
+    name: ast::Identifier<'src>,
+    parameters: Vec<TypeParameter<'src, A>>,
+}
+
+struct TypeParameter<'src, A: Annotation<'src>> {
+    name: A::TypeIdentifier,
+    kind: A::Kind,
+}
+
+struct Value<'src, A: Annotation<'src>> {
+    name: ast::Identifier<'src>,
+    typ: A::Type,
+}
+
+enum Type<Identifier, Child> {
+    Atom(Identifier),
+    Function {
+        parameter: Box<Child>,
+        return_type: Box<Child>,
+    },
+}
