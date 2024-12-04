62f0132004ae16cc22b29e2cfe81a25c7c035b12
Simon Bourne <simonbourne@gmail.com>
2024-12-04 15:29:40 +0000

    Update CI versions

--- packages/xtask/src/main.rs
+++ packages/xtask/src/main.rs
@@ -5,7 +5,14 @@ use xtask_base::{
 
 fn main() {
     CommonCmds::run(
-        CI::standard_workflow(StandardVersions::default(), &[]),
+        CI::standard_workflow(
+            StandardVersions {
+                rustc_stable_version: "1.83.0",
+                rustc_nightly_version: "nightly-2024-11-26",
+                udeps_version: "0.1.53",
+            },
+            &[],
+        ),
         |check| generate_open_source_files(2023, check),
     )
 }
