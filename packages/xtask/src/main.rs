537e8942a19eb1c3a2af2b1147b07232de3dba2e
Simon Bourne <simonbourne@gmail.com>
2025-03-23 14:08:48 +0000

    Update rust versions in CI

--- packages/xtask/src/main.rs
+++ packages/xtask/src/main.rs
@@ -8,8 +8,8 @@ fn main() {
     CommonCmds::run(
         CI::standard_workflow(
             StandardVersions {
-                rustc_stable_version: "1.85.0",
-                rustc_nightly_version: "nightly-2025-02-19",
+                rustc_stable_version: "1.85.1",
+                rustc_nightly_version: "nightly-2025-03-15",
                 udeps_version: "0.1.55",
             },
             &[],
