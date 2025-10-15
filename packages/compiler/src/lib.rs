93d715edb6c184be00140a577cdb1ffa9279af7e
Simon Bourne <simonbourne@gmail.com>
2025-10-15 16:29:09 +0100

    Compiler: Add a typing context

--- packages/compiler/src/lib.rs
+++ packages/compiler/src/lib.rs
@@ -3,7 +3,7 @@ use bandit_parser::{
     lex::{SrcToken, Token},
     parse::definitions,
 };
-use bandit_types::{Pretty, source::FunctionDefinition};
+use bandit_types::{context::Context, source::FunctionDefinition, Pretty};
 use winnow::Parser;
 
 pub fn compile(source: &str) -> Result<()> {
@@ -11,10 +11,12 @@ pub fn compile(source: &str) -> Result<()> {
     let source = definitions()
         .parse(&tokens)
         .map_err(|_| anyhow!("A parse error occurred"))?;
-
-    for FunctionDefinition { name, typ, .. } in source {
+    
+    for FunctionDefinition { name, typ, .. } in &source {
         println!("{name} : {}", typ.to_pretty_string(80));
     }
+    
+    let _ctx = Context::new(source.iter().map(|FunctionDefinition{name, typ, ..}| (*name, typ.clone())));
 
     Ok(())
 }
