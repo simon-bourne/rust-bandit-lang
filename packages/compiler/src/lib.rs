d680c3f5ccbc25ae7bb509e20a0793612471a39c
Simon Bourne <simonbourne@gmail.com>
2025-10-15 17:08:44 +0100

    Compiler: setup for type inference

--- packages/compiler/src/lib.rs
+++ packages/compiler/src/lib.rs
@@ -12,15 +12,45 @@ pub fn compile(source: &str) -> Result<()> {
         .parse(&tokens)
         .map_err(|_| anyhow!("A parse error occurred"))?;
 
-    for FunctionDefinition { name, typ, .. } in &source {
-        println!("{name} : {}", typ.to_pretty_string(80));
-    }
+    println!("Before type inference");
+    print_definitions(&source);
 
-    let _ctx = Context::new(
+    let mut ctx = Context::new(
         source
             .iter()
             .map(|FunctionDefinition { name, typ, .. }| (*name, typ.clone())),
     );
 
+    // TODO: We need to transform `FunctionDefinition<source::Term>` to
+    // `FunctionDefinition<linked::Term>`, which will have its types inferred
+    for FunctionDefinition { typ, value, .. } in &source {
+        if let Some(value) = value {
+            value
+                .clone()
+                .has_type(typ.clone())
+                .link(&mut ctx)
+                .map_err(|_| anyhow!("A link error occurred"))?;
+        }
+    }
+
+    ctx.constraints()
+        .solve()
+        .map_err(|_| anyhow!("A type inference error occurred"))?;
+
+    println!("After type inference");
+    print_definitions(&source);
+
     Ok(())
+}
+
+fn print_definitions(source: &[FunctionDefinition<'_>]) {
+    for FunctionDefinition { name, typ, value } in source {
+        print!("{name} : {}", typ.to_pretty_string(80));
+
+        if let Some(value) = value {
+            println!(" = {}", value.to_pretty_string(80));
+        } else {
+            println!();
+        }
+    }
 }
